f54bba7fa66c624e5b063d0b0527360ca97b6271
Pierrefeu Marius <pierrefeumarius@macbook-pro-de-marius.home>
2022-04-21 18:53:14 +0200

    page produit neuf

--- pageprincipal.php
+++ pageprincipal.php
@@ -2,22 +2,22 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Acceuil</title>
+  <title>Produits Neuf</title>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+
 </head>
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand px-2" href="#">Sneaker Studio</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Page d'acceuil</a>
+          <a class="nav-link" href="">Page d'acceuil</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Produits neuf</a>
@@ -46,6 +46,90 @@
   </nav>
 </header>
 <body>
-  
+  <br><br>
+<div class="row mt-4 px-2">
+  <div class="card" style="width: 18rem;">
+    <img src="Air-Jordan-1-Retro-High-White-University-Blue-Black-Product.jpg.webp" class="card-img-top mt-5" height="150" alt="..."><br><br>
+    <div class="card-body">
+      <h4 class="card-title">Jordan 1</h4><br>
+      <p class="card-text">Jordan 1 Retro High White University Blue Black (DS)</p>
+      <a href="#" class="btn btn-primary">347 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Air-Jordan-1-Mid-Chicago-Toe-Product.jpg.webp" class="card-img-top mt-5" height="150" alt="..."><br><br>
+    <div class="card-body">
+      <h4 class="card-title">Jordan 1</h4><br>
+      <p class="card-text">Nike Jordan 1 Mid Chicago Toe (DS)</p>
+      <a href="#" class="btn btn-primary">210 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Balenciaga-Track-White-Black.jpg.webp" class="card-img-top mt-4" height="200" alt="..."><br>
+    <div class="card-body">
+      <h4 class="card-title">Balenciaga Track</h4><br>
+      <p class="card-text">Balenciaga Track White Black (DS)</p>
+      <a href="#" class="btn btn-primary">848 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Nike-Vaporwaffle-Sacai-White-Gum-Product.jpg.webp" class="card-img-top mt-5" height="150" alt="..."><br><br>
+    <div class="card-body">
+      <h4 class="card-title">Nike Vaporwaffle</h4><br>
+      <p class="card-text">Nike Vaporwaffle Sacai Sail Gum (DS)</p>
+      <a href="#" class="btn btn-primary">419 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Nike-Dunk-Low-Off-White-Lot-26-Product.jpg.webp" class="card-img-top mt-5" height="150" alt="..."><br><br>
+    <div class="card-body">
+      <h4 class="card-title">Nike Dunk Low</h4><br>
+      <p class="card-text">Nike Dunk Low Off-White Lot 26 (DS)</p>
+      <a href="#" class="btn btn-primary">492 €</a>
+    </div>
+  </div>
+</div><br>
+<div class="row mt-5 px-2">
+  <div class="card" style="width: 18rem;">
+    <img src="New-Balance-550-Aime-Leon-Dore-White-Green-Product.jpg.webp" class="card-img-top" height="190" alt="..."><br><br>
+    <div class="card-body">
+      <h5 class="card-title">Jordan 1</h5><br>
+      <p class="card-text">Jordan 1 Retro High White University Blue Black (DS)</p>
+      <a href="#" class="btn btn-primary">347 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Nike-Dunk-Low-Off-White-Lot-50-V2-Product.jpg.webp" class="card-img-top" alt="..."><br><br>
+    <div class="card-body">
+      <h5 class="card-title">Jordan 1</h5><br>
+      <p class="card-text">Nike Jordan 1 Mid Chicago Toe (DS)</p>
+      <a href="#" class="btn btn-primary">210 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Nike-LD-Waffle-SF-Sacai-fragment-Grey-Product.jpg.webp" class="card-img-top" alt="..."><br><br>
+    <div class="card-body">
+      <h5 class="card-title">Balenciaga Track</h5>
+      <p class="card-text">Balenciaga Track White Black (DS)</p>
+      <a href="#" class="btn btn-primary">848 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Air-Jordan-4-Retro-White-Oreo-2021-Product.jpg.webp" class="card-img-top" alt="..."><br><br>
+    <div class="card-body">
+      <h5 class="card-title">Nike Vaporwaffle</h5>
+      <p class="card-text">Nike Vaporwaffle Sacai Sail Gum (DS)</p>
+      <a href="#" class="btn btn-primary">419 €</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="Air-Jordan-1-Low-fragment-design-x-Travis-Scott-Product.jpg.webp" class="card-img-top" alt="..."><br><br>
+    <div class="card-body">
+      <h5 class="card-title">Nike Dunk Low</h5>
+      <p class="card-text">Nike Dunk Low Off-White Lot 26 (DS)</p>
+      <a href="#" class="btn btn-primary">492 €</a>
+    </div>
+  </div>
+</div>
 </body>
 </html>
